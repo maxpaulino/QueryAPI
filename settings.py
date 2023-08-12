178f48db7497879f8c3792d8318490141e707b40
Max Paulino <max.paulinor@gmail.com>
2023-08-12 14:00:51 -0600

    Added server port to settings.py

--- settings.py
+++ settings.py
@@ -17,6 +17,7 @@ conn = pyodbc.connect(
     f"Database={os.getenv('DB_NAME')};"
     f"UID={os.getenv('DB_USERNAME')};"
     f"PWD={os.getenv('DB_PASSWORD')};"
+    f"PORT={os.getenv('DB_PORT')};"
 )
 
 openai.api_key = os.environ.get('OPENAI_API_KEY')
